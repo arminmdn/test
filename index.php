a10036550365be1d2ac42370d5592183dfbec842
A R M I N <arminmadani@rocketmail.com>
2020-05-11 16:25:00 +0430

    this is the first

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//
+//$a=50+6;
+//$b=50*6;
+//$c=50/6;
+//
+//echo("$a");
+//echo "<br>";
+//echo ("$b" );
+//echo "<br>";
+//echo ("$c");
+$a=10;
+if($a>10){
+    echo "greater than 10";
+
+}
+else{
+    echo "less than 10";
+}
+echo "<br>";
+echo "<br>";
+$b=20;
+if($b<20){
+    echo "less than 20";
+}
+else{
+    echo "greater than 20";
+}
+
+
+
+
+?>
